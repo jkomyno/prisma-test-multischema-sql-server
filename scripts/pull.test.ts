11d3c4c34c6754c7f1dc3c88784a0bdc7f1a2bd1
jkomyno <skiabo97@gmail.com>
2022-08-29 17:07:24 +0200

    test: add --force arg to db pull test

--- scripts/pull.test.ts
+++ scripts/pull.test.ts
@@ -3,9 +3,20 @@ import execa from 'execa'
 import assert from 'node:assert'
 
 describe('db pull', () => {
-  test('', async () => {
+  test('vanilla', async () => {
     try {
       await execa('prisma', ['db', 'pull', '--print', `--url="${process.env.DATABASE_URL}"`])
+      assert.deepEqual(true, false) // trigger error if we reach this line
+    } catch (error) {
+      const e = error as execa.ExecaError
+      assert(e.message.includes('Error: P1012 Introspection failed as your current Prisma schema file is invalid'))
+    }
+  })
+
+  test('--force', async () => {
+    try {
+      await execa('prisma', ['db', 'pull', '--print', `--url="${process.env.DATABASE_URL}"`])
+      assert.deepEqual(true, false) // trigger error if we reach this line
     } catch (error) {
       const e = error as execa.ExecaError
       assert(e.message.includes('Error: P1012 Introspection failed as your current Prisma schema file is invalid'))
